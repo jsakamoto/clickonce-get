f1fc1abb03f3987486e9b1c41d3c1520b3f62321
jsakamoto <junichi.jp@gmail.com>
2014-05-23 22:13:57 +0900

    testing.

--- ClickOnceGet/App_Start/FilterConfig.cs
+++ ClickOnceGet/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace ClickOnceGet
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
