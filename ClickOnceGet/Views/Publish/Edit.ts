2e1bd6c9a39dea7b8e8573dc03747e6e1de814a7
jsakamoto <junichi.jp@gmail.com>
2015-03-29 21:43:09 +0900

    Improve App Info Editor: add char counter at title and description text box.

---
+++ ClickOnceGet/Views/Publish/Edit.ts
@@ -0,0 +1,19 @@
+﻿$('*[ng-model]').toArray().map(elem => {
+    $(elem).attr('ng-init', $(elem).attr('name') + '= \'' + $(elem).val() + '\'');
+});
+
+
+module Publish {
+    var app = angular.module('Publish', []);
+
+    app.filter('charcounter',() => (text: string, max: number) =>
+        (max - (text || '').length).toString());
+
+    export class EditController {
+        constructor($scope: any) {
+            $scope.greeting = { text: 'Hello' };
+        }
+    }
+
+    app.controller('EditController', EditController);
+}
