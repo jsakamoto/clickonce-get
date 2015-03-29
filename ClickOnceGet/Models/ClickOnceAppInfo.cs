212009817341510e8865f5b3ecae142ebbe1999b
jsakamoto <junichi.jp@gmail.com>
2015-03-29 20:45:01 +0900

    Fix: app crash when editing app info entering a text it looks like HTML.

--- ClickOnceGet/Models/ClickOnceAppInfo.cs
+++ ClickOnceGet/Models/ClickOnceAppInfo.cs
@@ -4,13 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ClickOnceGet.Models
 {
     [DataContract]
     public class ClickOnceAppInfo
     {
-        [DataMember]
+        [DataMember, AllowHtml]
         public string Name { get; set; }
 
         public string OwnerId { get; set; }
@@ -18,16 +19,16 @@ namespace ClickOnceGet.Models
         [DataMember]
         public DateTime RegisteredAt { get; set; }
 
-        [DataMember, StringLength(140)]
+        [DataMember, StringLength(140), AllowHtml]
         public string Title { get; set; }
 
-        [DataMember, StringLength(140)]
+        [DataMember, StringLength(140), AllowHtml]
         public string Description { get; set; }
 
         [DataMember, Url]
         public string ProjectURL { get; set; }
 
-        [DataMember]
+        [DataMember, AllowHtml]
         public string PublisherName { get; set; }
 
         [DataMember]
